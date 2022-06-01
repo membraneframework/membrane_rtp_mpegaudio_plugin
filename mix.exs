c47919e6d516b8ba6db6544bcc6e4080b54a631d
Michał Śledź <michalsledz34@gmail.com>
2022-06-01 14:03:17 +0200

    Update deps. Bump version to 0.11.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.RTP.MPEGAudio.MixProject do
   use Mix.Project
 
-  @version "0.10.0"
+  @version "0.11.0"
   @github_url "https://github.com/membraneframework/membrane_rtp_mpegaudio_plugin"
 
   def project do
@@ -57,7 +57,7 @@ defmodule Membrane.RTP.MPEGAudio.MixProject do
   defp deps do
     [
       {:membrane_core, "~> 0.10.0"},
-      {:membrane_rtp_format, "~> 0.4.0"},
+      {:membrane_rtp_format, "~> 0.5.0"},
       {:membrane_caps_audio_mpeg, "~> 0.2.0"},
 
       # dev
