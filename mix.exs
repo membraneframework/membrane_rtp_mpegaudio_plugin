ecadbc97a44490991eeff6df752db891223d9ca3
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2026-06-29 11:27:51 +0200

    Set ex_doc dependency to >= 0.40.0

--- mix.exs
+++ mix.exs
@@ -76,7 +76,7 @@ defmodule Membrane.RTP.MPEGAudio.MixProject do
       {:membrane_mpegaudio_format, "~> 0.3.0"},
 
       # dev
-      {:ex_doc, "~> 0.40", only: :dev, runtime: false},
+      {:ex_doc, ">= 0.40.0", only: :dev, runtime: false},
       {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
       {:credo, ">= 0.0.0", only: :dev, runtime: false}
     ]
